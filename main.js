e6e2777948b5e7453f6b7aed6f3f17ceec2a1f81
nicobreins <bharatswaroop98@gmail.com>
2022-02-21 22:18:45 +0530

    making bot smart

--- main.js
+++ main.js
@@ -66,6 +66,38 @@ for(let i = boxEl.length -1; i >= 0; i--){
                     boxEl[4].innerText = 'X';
                 }else if(boxEl[2].innerText == boxEl[6].innerText && boxEl[4].innerText == '' && boxEl[2].innerText !== ''){
                     boxEl[4].innerText = 'X';
+                }else if(boxEl[0].innerText == boxEl[1].innerText && boxEl[2].innerText == '' && boxEl[0].innerText !== ''){
+                    boxEl[2].innerText = 'X';
+                }else if(boxEl[3].innerText == boxEl[4].innerText && boxEl[5].innerText == '' && boxEl[3].innerText !== ''){
+                    boxEl[5].innerText = 'X';
+                }else if(boxEl[6].innerText == boxEl[7].innerText && boxEl[8].innerText == '' && boxEl[6].innerText !== ''){
+                    boxEl[8].innerText = 'X';
+                }else if(boxEl[0].innerText == boxEl[3].innerText && boxEl[6].innerText == '' && boxEl[0].innerText !== ''){
+                    boxEl[6].innerText = 'X';
+                }else if(boxEl[1].innerText == boxEl[4].innerText && boxEl[7].innerText == '' && boxEl[1].innerText !== ''){
+                    boxEl[7].innerText = 'X';
+                }else if(boxEl[2].innerText == boxEl[5].innerText && boxEl[8].innerText == '' && boxEl[2].innerText !== ''){
+                    boxEl[8].innerText = 'X';
+                }else if(boxEl[0].innerText == boxEl[4].innerText && boxEl[8].innerText == '' && boxEl[0].innerText !== ''){
+                    boxEl[8].innerText = 'X';
+                }else if(boxEl[2].innerText == boxEl[4].innerText && boxEl[6].innerText == '' && boxEl[2].innerText !== ''){
+                    boxEl[6].innerText = 'X';
+                }else if(boxEl[2].innerText == boxEl[1].innerText && boxEl[0].innerText == '' && boxEl[2].innerText !== ''){
+                    boxEl[0].innerText = 'X';
+                }else if(boxEl[5].innerText == boxEl[4].innerText && boxEl[3].innerText == '' && boxEl[5].innerText !== ''){
+                    boxEl[3].innerText = 'X';
+                }else if(boxEl[8].innerText == boxEl[7].innerText && boxEl[6].innerText == '' && boxEl[8].innerText !== ''){
+                    boxEl[6].innerText = 'X';
+                }else if(boxEl[6].innerText == boxEl[3].innerText && boxEl[0].innerText == '' && boxEl[6].innerText !== ''){
+                    boxEl[0].innerText = 'X';
+                }else if(boxEl[7].innerText == boxEl[4].innerText && boxEl[1].innerText == '' && boxEl[7].innerText !== ''){
+                    boxEl[1].innerText = 'X';
+                }else if(boxEl[8].innerText == boxEl[5].innerText && boxEl[2].innerText == '' && boxEl[8].innerText !== ''){
+                    boxEl[2].innerText = 'X';
+                }else if(boxEl[8].innerText == boxEl[4].innerText && boxEl[0].innerText == '' && boxEl[8].innerText !== ''){
+                    boxEl[0].innerText = 'X';
+                }else if(boxEl[6].innerText == boxEl[4].innerText && boxEl[2].innerText == '' && boxEl[6].innerText !== ''){
+                    boxEl[2].innerText = 'X';
                 }else{
                     let cpuInputVal = Math.floor((Math.random() * 10) % 9);
                     if(boxEl[cpuInputVal].innerText == ''){
